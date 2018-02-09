cc2efa8d895a2b0bba31852e19b0fccf7998b9f7
Keivio OLiveira <keivio.cardoso@rgsistemas.com.br>
2018-02-09 14:49:08 -0200

    fix babelrc conflict with no exports defined

--- vue-datepicker.vue
+++ vue-datepicker.vue
@@ -373,18 +373,13 @@ table {
 </template>
 <script>
 'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
 var _moment = require('moment');
 
 var _moment2 = _interopRequireDefault(_moment);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = {
+export default = {
   props: {
     required: false,
     date: {
